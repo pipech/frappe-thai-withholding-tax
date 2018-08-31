3d3b90ecf18f3d8e8a3a7344d52d03885982a69e
pipeCh <pipech@gmail.com>
2018-08-31 14:57:58 +0700

    added disable signup function

--- thai_wht/utils/__init__.py
+++ thai_wht/utils/__init__.py
@@ -6,3 +6,16 @@ def add_system_manager(email, pwd):
     from frappe.utils.user import add_system_manager
     add_system_manager(email=email)
     update_password(user=email, pwd=pwd)
+
+
+def disable_signup():
+    disable_signup_sql = """
+        UPDATE `tabSingles`
+        SET
+            `value`='1'
+        WHERE
+            `doctype`='Website Settings'
+            AND
+            `field`='disable_signup'
+        ;"""
+    frappe.db.sql(disable_signup_sql, auto_commit=1)
