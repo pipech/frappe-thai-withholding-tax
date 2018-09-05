9c789d421034a1b4d5d1ae1fa617ac9cffc0017c
pipeCh <pipech@gmail.com>
2018-09-05 12:10:52 +0700

    added set db host function

--- thai_wht/utils/__init__.py
+++ thai_wht/utils/__init__.py
@@ -19,3 +19,34 @@ def disable_signup():
             `field`='disable_signup'
         ;"""
     frappe.db.sql(disable_signup_sql, auto_commit=1)
+
+
+def set_db_host():
+    """Preventing lost mysql connection when frappe image change ip addr."""
+    import subprocess
+    db_host = frappe.conf.db_host
+    root_pwd = frappe.conf.root_password
+    db_name = frappe.conf.get('db_name')
+    update_sql = """
+        UPDATE mysql.db
+        SET host = '10.%.%.%'
+        WHERE host LIKE '10.%.%.%' AND user = '{db_name}';
+
+        UPDATE mysql.user
+        SET host = '10.%.%.%'
+        WHERE host LIKE '10.%.%.%' AND user = '{db_name}';
+    """.format(db_name=db_name)
+    process = subprocess.Popen(
+        [
+            'mysql',
+            '-h{}'.format(db_host),
+            '-uroot',
+            '-p{}'.format(root_pwd),
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        stdin=subprocess.PIPE
+        )
+    out, err = process.communicate(update_sql)
+    if process.returncode != 0:
+        raise Exception('Something failed : {}'.format(err))
