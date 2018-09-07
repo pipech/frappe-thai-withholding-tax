7819c03bb21937b29c7f61385aa1943c6c82fd4d
pipeCh <pipech@gmail.com>
2018-09-07 10:09:11 +0700

    added wht cert icon

--- thai_wht/config/desktop.py
+++ thai_wht/config/desktop.py
@@ -2,13 +2,23 @@
 from __future__ import unicode_literals
 from frappe import _
 
+
 def get_data():
-	return [
-		{
-			"module_name": "Thai Wht",
-			"color": "green",
-			"icon": "octicon octicon-repo-force-push",
-			"type": "module",
-			"label": _("Thai Wht")
-		}
-	]
+    return [
+        {
+            'module_name': 'Thai Wht',
+            'color': 'green',
+            'icon': 'octicon octicon-repo-force-push',
+            'type': 'module',
+            'label': _('Thai Wht')
+        },
+        {
+            'module_name': 'Wht Cert',
+            'color': 'green',
+            'icon': 'octicon octicon-file-code',
+            'type': 'link',
+            '_doctype': 'Wht Cert',
+            'link': 'List/Wht Cert',
+            'label': _('Wht Cert')
+        },
+    ]
